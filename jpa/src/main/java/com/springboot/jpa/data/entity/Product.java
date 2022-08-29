3655c391e79bd20d5df2721b5686a761713b6e9c
이상훈 <huny22@etribe.co.kr>
2022-08-29 12:18:14 +0900

    220829_jpa_end

--- jpa/src/main/java/com/springboot/jpa/data/entity/Product.java
+++ jpa/src/main/java/com/springboot/jpa/data/entity/Product.java
@@ -9,7 +9,22 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+// DB의 테이블구조를 생성하는 데 사용
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor // 매개변수가 없는 생성자를 자동 생성
+@AllArgsConstructor // 모든 필드를 매개변수로 갖는 생성자를 자동 생성
+// @RequiredArgsConstructor 필드 중 final이나 @NotNull이 설정된 변수를 매개변수로 갖는 생성자를 자동 생성
+// @toString 필드의 값을 문자열로 조합해서 리턴, 숨겨야 할 정보가 있으면 (exclude = "name")속성을 사용해 필드를 자동 생성에서 제외
+// @EqualsAndHashCode 객체의 동등성(Equality)과 동일성(Dientity)을 비교하는 연산 메서드를 생성
+// @Data - @Getter/@Setter/@RequiredArgsConstructor, @ToString, @EqualsAndHashCode를 모두 포괄하는 어노테이션
 @Table(name = "product")
 public class Product {
 
@@ -29,53 +44,5 @@ public class Product {
 	private LocalDateTime createAt;
 
 	private LocalDateTime updateAt;
-
-	public Long getNumber() {
-		return number;
-	}
-
-	public void setNumber(Long number) {
-		this.number = number;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Integer getPrice() {
-		return price;
-	}
-
-	public void setPrice(Integer price) {
-		this.price = price;
-	}
-
-	public Integer getStock() {
-		return stock;
-	}
-
-	public void setStock(Integer stock) {
-		this.stock = stock;
-	}
-
-	public LocalDateTime getCreateAt() {
-		return createAt;
-	}
-
-	public void setCreateAt(LocalDateTime createAt) {
-		this.createAt = createAt;
-	}
-
-	public LocalDateTime getUpdateAt() {
-		return updateAt;
-	}
-
-	public void setUpdateAt(LocalDateTime updateAt) {
-		this.updateAt = updateAt;
-	}
 
 }
--- jpa/src/main/java/com/springboot/jpa/data/repository/ProductRepository.java
+++ jpa/src/main/java/com/springboot/jpa/data/repository/ProductRepository.java
@@ -3,7 +3,7 @@ package com.springboot.jpa.data.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springboot.jpa.data.entity.Product;
-
+// Entity가 생성한 DB에 접근할 때 사용 <대상엔티티, @Id>
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
